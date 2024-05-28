3927bd76560ad67612e84419289ba2872f38a692
Diego Guimarães <diegomsg@gmail.com>
2024-05-28 00:20:42 -0300

    working tidy cobrancas

--- fun/fun2.R
+++ fun/fun2.R
@@ -10,6 +10,11 @@ require(tidyr)
 require(purrr)
 
 
+# parametros --------------------------------------------------------------
+
+br_locale <- readr::locale(decimal_mark = ",", grouping_mark = ".")
+
+
 # funcoes -----------------------------------------------------------------
 
 load_acordo_file <- function(file) {
@@ -44,8 +49,7 @@ partition_acordos_l2 <- function(data) {
     tibble = is.data.frame(data),
     valida = valida_xlsx_cells(data)
   )
-  l2_corners_text <- c("Detalhes", "Cobranças originais", 
-                       "Acréscimos", "Parcelas do acordo")
+  l2_corners_text <- c("Detalhes", "Cobranças originais", "Parcelas do acordo")
   l2_corners <- filter(data, character %in% l2_corners_text)
   partition(data, l2_corners) |>
     select(name = character, cells)
@@ -61,6 +65,7 @@ tidy_detalhes <- function(cells) {
   stopifnot(
     check_block(cells, "Detalhes")
   )
+  
   cells |>
     slice(-1) |>
     behead(direction = "left", tipo) |> 
@@ -83,6 +88,46 @@ tidy_det_acordos <- function(blocos) {
     tidy_detalhes()
 }
 
+tidy_cobrancas <- function(cells) {
+  # arruma bloco cobrancas
+  # 1 linha de saída
+  stopifnot(
+    check_block(cells, "Cobranças originais")
+  )
+  
+  acrescimos <- cells |>
+    slice_tail(n = 4) |>
+    behead(direction = "left", header) |>
+    select(-(row:content)) |>
+    spatter(header) |> 
+    janitor::clean_names() |>
+    select(acrescimos, total_devido) |>
+    mutate(across(everything(), ~readr::parse_number(.x, locale = br_locale)))
+  
+  cells |>
+    slice(-1) |>
+    slice(1:(n()-5)) |> 
+    behead(direction = "up", header) |> 
+    select(row, data_type, numeric, character, header) |>
+    spatter(header) |> 
+    select(-row) |>
+    janitor::clean_names() |>
+    mutate(competencia = lubridate::my(competencia),
+           composicao = as.numeric(sub(",", ".", composicao, fixed = TRUE)),
+           vencimento = lubridate::dmy(vencimento)) |>
+    fill(everything()) |> 
+    nest(cobrancas = everything()) |>
+    bind_cols(acrescimos)
+}
+
+tidy_cob_acordos <- function(blocos) {
+  # arruma bloco cobranças a partir de blocos em nested list of tbl
+  # 1 tbl = 1 line
+  pull(blocos, cells) |>
+    list_pick(2) |>
+    tidy_cobrancas()
+}
+
 # example -----------------------------------------------------------------
 
 files <- fs::path("data") |>
@@ -99,24 +144,41 @@ data_pipe <- load_acordo_file(ex_file) |>
   partition_acordos() |>
   mutate(blocos = map(acordo_full_data, partition_acordos_l2)) |>
   select(-acordo_full_data) |>
-  mutate(detalhes = map(blocos, tidy_det_acordos)) |> 
-  unnest(detalhes)
+  mutate(detalhes = map(blocos, tidy_det_acordos),
+         cobrancas = map(blocos, tidy_cob_acordos)) |> 
+  unnest(c(detalhes, cobrancas))
 
 # teste parcial
-#TODO: COBRANÇAS
-ex_cobrancas <- data_pipe$cells[[1]][[2]]
+#TODO: PARCELAS
+ex_parcelas <- data_pipe$blocos[[1]] |>
+  pull(cells) |>
+  list_pick(3)
 
-# detalhes
-check_block(ex_detalhe, "Detalhes")
-ex_detalhe |>
+# parcelas
+check_block(ex_cobrancas, "Cobranças originais")
+ex_cobrancas |>
   slice(-1) |>
-  behead(direction = "left", tipo) |> 
-  select(tipo, character) |> 
-  pivot_wider(names_from = tipo, values_from = character) |>
+  slice(1:(n()-5)) |> 
+  behead(direction = "up", header) |> 
+  select(row, data_type, numeric, character, header) |>
+  spatter(header) |> 
+  select(-row) |>
   janitor::clean_names() |>
-  mutate(cod_acordo = as.integer(cod_acordo),
-         efetuado_em = lubridate::dmy(efetuado_em))
-  # pegar pares de linhas e separar em 2 colunas
+  mutate(competencia = lubridate::my(competencia),
+         composicao = as.numeric(sub(",", ".", composicao, fixed = TRUE)),
+         vencimento = lubridate::dmy(vencimento)) |>
+  fill(everything()) |> 
+  nest(cobrancas = everything()) |>
+  bind_cols(acrescimos) |> View()
+
+acrescimos <- ex_cobrancas |>
+  slice_tail(n = 4) |>
+  behead(direction = "left", header) |>
+  select(-(row:content)) |>
+  spatter(header) |> 
+  janitor::clean_names() |>
+  select(acrescimos, total_devido) |>
+  mutate(across(everything(), ~readr::parse_number(.x, locale = br_locale)))
 data_pipe$name[1]
 
-
+tidy_cobrancas(ex_cobrancas)
